4d32a3c503b5986e29fb9d43dcebbd335ab10e1b
Tom George <tg82490@gmail.com>
2014-03-06 21:49:07 -0500

    Finished Minesweeper

--- src/main/java/com/tgeorge/challenges/Chapter1.java
+++ src/main/java/com/tgeorge/challenges/Chapter1.java
@@ -28,7 +28,7 @@ public class Chapter1
 
 	public static void minesweeper(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new FileReader(args[0]));
-		int[][] field;
+		int[][] field = null;
 		int height = -1, width = -1;
 		int curRow = 0;
 		String tmp;
@@ -37,14 +37,114 @@ public class Chapter1
 						String[] nums = tmp.split(" ");
 						height = new Integer(nums[0]).intValue();
 						width = new Integer(nums[1]).intValue();
-				} else {
 						field = new int[height][width];
+				} else {
+					for(int i = 0; i < width; i++) {
+						String tmpChar = tmp.substring(i, i+ 1);
+						if (tmpChar.equals("*"))
+								field[curRow][i] = -1;
+						else
+								field[curRow][i] = 0;
+					}
+					curRow++;
 				}	
-
 		}
 		in.close();
+		fill_adjacent(field, height, width);
+		for(int i = 0; i < height; i++) {
+				for(int j = 0; j < width; j++){
+						if(mine(field, i, j)) {
+								System.out.print("* ");
+						} else {
+								System.out.print(field[i][j] + " ");
+						}
+				}
+				System.out.println();
+			}
 	}
-			
+
+	public static void fill_top_left(int[][] field, int i, int j)
+	{
+			fill(field, i - 1, j - 1);
+	}
+	
+	public static void fill_top(int[][] field, int i, int j)
+	{
+			fill(field, i - 1, j);
+	}
+
+	public static void fill_top_right(int[][] field, int i, int j)
+	{
+			fill(field, i - 1, j + 1);
+	}
+
+	public static void fill_left(int[][] field, int i, int j)
+	{
+			fill(field, i, j - 1);
+	}
+
+	public static void fill_right(int[][] field, int i, int j)
+	{
+			fill(field, i, j + 1);
+	}
+
+	public static void fill_bottom_right(int[][] field, int i, int j)
+	{
+			fill(field, i + 1, j + 1);
+	}
+
+	public static void fill_bottom_left(int[][] field, int i, int j)
+	{
+			fill(field, i + 1, j - 1);
+	}
+
+	public static void fill_bottom(int[][] field, int i, int j)
+	{
+			fill(field, i + 1, j);
+	}
+
+	public static void fill_adjacent(int [][] field, int height, int width) 
+	{
+			for(int i = 0; i < height; i++) {
+					for(int j = 0; j < width; j++) {
+							if(mine(field, i, j)) {
+								if(i == 0) {
+										if( j != 0)
+												fill_left(field, i, j);
+										if( j != width - 1)
+												fill_right(field, i, j);
+								}
+								if(i != 0) {
+										fill_left(field, i, j);	
+										if(j != 0)
+												fill_top_left(field, i, j);
+										if(j != width - 1)
+												fill_top_right(field, i, j);
+								fill_top(field, i, j);
+								}
+								if(i != height - 1) {
+										fill_bottom(field, i, j);
+										if(j != 0)
+												fill_bottom_left(field, i, j);
+										if(j != width - 1)
+												fill_bottom_right(field, i, j);
+								}
+							}
+					}
+			}
+	}
+
+	public static void fill(int[][] field, int i, int j)
+	{
+			if(!mine(field, i, j))
+					field[i][j]++;
+	}
+
+	public static boolean mine(int[][] field, int i, int j)
+	{
+			return field[i][j] == -1;
+	}
+
 	public static void main(String[] args) throws IOException {
 		//threenplusone(args);
 		minesweeper(args);
